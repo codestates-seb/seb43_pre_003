500d4f7ebc7d190fd22748b156a06fbd2f0a417a
JSeungHyun <83762088+JSeungHyun@users.noreply.github.com>
2023-04-14 13:10:38 +0900

    start to develop

---
+++ src/test/java/com/seb43/preProject/PreProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seb43.preProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
