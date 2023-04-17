7b68cfa6e65dc5f7c325b4406e818e616b3c7afa
JoDaeUk <du39574@gmail.com>
2023-04-17 22:22:55 +0900

    프로필 조회 구현

--- server/src/main/java/com/seb43/preProject/member/controller/MemberController.java
+++ server/src/main/java/com/seb43/preProject/member/controller/MemberController.java
@@ -5,23 +5,23 @@ import com.seb43.preProject.member.dto.MemberPostDto;
 import com.seb43.preProject.member.entity.Member;
 import com.seb43.preProject.member.mapper.MemberMapper;
 import com.seb43.preProject.member.service.MemberService;
+import com.seb43.preProject.response.SingleResponseDto;
 import com.seb43.preProject.utils.URICreator;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import javax.validation.constraints.Positive;
 import java.net.URI;
 
 @RestController
 @RequestMapping("/members")
 @Validated
+@Slf4j
 public class MemberController {
     private final static String MEMBER_DEFAULT_URL = "/members";
     private final MemberService memberService;
@@ -38,6 +38,14 @@ public class MemberController {
         URI location = URICreator.createUri(MEMBER_DEFAULT_URL, member.getMemberId());
 
         return ResponseEntity.created(location).build();
+    }
+    @GetMapping("/{member-id}/profile")
+    public ResponseEntity getMember(@PathVariable("member-id") @Positive long memberId){
+        Member member = memberService.findVerifiedMember(memberId);
+
+        return new ResponseEntity<>(
+                new SingleResponseDto<>(mapper.memberToMemberResponse(member)),HttpStatus.OK);
+
     }
 
 }
--- server/src/main/java/com/seb43/preProject/member/mapper/MemberMapper.java
+++ server/src/main/java/com/seb43/preProject/member/mapper/MemberMapper.java
@@ -1,10 +1,14 @@
 package com.seb43.preProject.member.mapper;
 
+import com.seb43.preProject.member.dto.MemberDto;
 import com.seb43.preProject.member.dto.MemberPostDto;
 import com.seb43.preProject.member.entity.Member;
 import org.mapstruct.Mapper;
+import org.springframework.stereotype.Service;
 
+@Service
 @Mapper(componentModel = "spring")
 public interface MemberMapper {
     Member memberPostDtoToMember(MemberPostDto memberPostDto);
+    MemberDto.response memberToMemberResponse(Member member);
 }
