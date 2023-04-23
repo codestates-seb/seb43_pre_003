a22cfe572fde784a3839b3237189e0d0886eb796
nile27 <ddj03104@gmail.com>
2023-04-23 10:42:28 +0900

    feat: 로그인, 회원가입

--- client/src/Pages/Login.js
+++ client/src/Pages/Login.js
@@ -1,12 +1,12 @@
 import styled from "styled-components";
-import Header from "../Components/Header";
 import Logoimg from "../Components/style/img/logo-stackof.png";
 import Input from "../Components/style/Input";
 import Button from "../Components/style/Button";
 import Sign from "../Components/style/img/sign.png";
 import Oauth from "../Components/OauthBtn";
-import { Link } from "react-router-dom";
-//import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
+import axios from "axios";
 
 const Container = styled.div`
   display: flex;
@@ -28,7 +28,7 @@ const Emailbox = styled.div`
   flex-direction: column;
   background: var(--white);
   width: 288px;
-  height: 250px;
+  height: height: ${(p) => p.height || "250px"};
   padding: 24px;
   margin: 24px 0;
   box-shadow: 0px 1px 2px rgba(0, 0, 0, 0.05), 0px 1px 4px rgba(0, 0, 0, 0.05),
@@ -75,37 +75,135 @@ const Signimg = styled.img`
   color: blue;
 `;
 
-function Login() {
+const Errdiv = styled.div`
+  padding-top: 5px;
+  color: var(--red-400);
+  font-size: var(--font-small);
+`;
+
+function Login({ setAuth }) {
+  const navi = useNavigate();
+  const [check, setCheck] = useState(true);
+  const [loginInfo, setLoginInfo] = useState({
+    email: "",
+    password: "",
+  });
+  const [errMessage, setErrMessage] = useState("");
+  const [errpw, setErrpw] = useState("");
+  const [count, setCount] = useState(true);
+
+  const handleInputValue = (key) => (e) => {
+    console.log({ ...loginInfo, [key]: e.target.value });
+    setLoginInfo({ ...loginInfo, [key]: e.target.value });
+  };
+
+  const funcLogin = () => {
+    axios.defaults.withCredentials = true;
+
+    // const headers = {
+    //   "Access-Control-Allow-Origin": "*",
+    //   "Content-Type": "application/json",
+    // };
+
+    if (!loginInfo.email) {
+      setErrMessage("아이디를 입력하세요.");
+      setCheck(false);
+      return;
+    }
+
+    if (!loginInfo.password) {
+      setErrpw("비밀번호를 입력하세요.");
+      setCheck(false);
+      return;
+    }
+
+    return axios
+      .post("http://localhost:3001/member", { loginInfo }, {})
+      .then((res) => {
+        console.log(res.data);
+        if (res.data.loginInfo.email === loginInfo.email) {
+          setAuth(true);
+          setErrMessage("");
+          setErrpw("");
+          navi("/");
+          setCount(true);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        setCount(false);
+        setErrMessage("이메일 또는 패스워드가 올바르지 않습니다.");
+      });
+  };
+
   return (
     <>
-      <Header />
       <Container>
         <Logo src={Logoimg} alt=""></Logo>
         <Oauth />
-        <Emailbox>
+        <Emailbox height={check ? "250px" : "320px"}>
           <Eldiv>
             <EmailSpan>Email</EmailSpan>
           </Eldiv>
-          <Input type="text" errorType="default"></Input>
-          <Eldiv>
-            <Passworddiv>
-              <EmailSpan>Password</EmailSpan>
-              <Link to="/Forget">
-                <ForgetBtn>Forgot password?</ForgetBtn>
-              </Link>
-            </Passworddiv>
-            <Input type="Password" errorType="default"></Input>
-          </Eldiv>
-          <Button
-            size="custom"
-            variant="mediumBlue"
-            width="242px"
-            height="35px"
-            padding="10px 10px 10px 10px"
-            margin="20px 0px 20px 0px"
-          >
-            Log in
-          </Button>
+          <form onSubmit={(e) => e.preventDefault()}>
+            {loginInfo.email.length === 0 && count ? (
+              <Input
+                type="text"
+                id="email"
+                errorType="Login"
+                onChange={handleInputValue("email")}
+              />
+            ) : (
+              <>
+                <Input
+                  type="text"
+                  id="email"
+                  errorType="error"
+                  onChange={handleInputValue("email")}
+                />
+                <Errdiv>{errMessage}</Errdiv>
+              </>
+            )}
+
+            <Eldiv>
+              <Passworddiv>
+                <EmailSpan>Password</EmailSpan>
+                <Link to="/Forget">
+                  <ForgetBtn>Forgot password?</ForgetBtn>
+                </Link>
+              </Passworddiv>
+              {loginInfo.password.length === 0 && count ? (
+                <Input
+                  type="password"
+                  id="password"
+                  errorType="Login"
+                  onChange={handleInputValue("password")}
+                />
+              ) : (
+                <>
+                  <Input
+                    type="password"
+                    id="password"
+                    errorType="error"
+                    onChange={handleInputValue("password")}
+                  />
+                  <Errdiv>{errpw}</Errdiv>
+                </>
+              )}
+            </Eldiv>
+            <Button
+              type="submit"
+              size="custom"
+              variant="mediumBlue"
+              width="242px"
+              height="35px"
+              padding="10px 10px 10px 10px"
+              margin="20px 0px 20px 0px"
+              onClick={funcLogin}
+            >
+              Log in
+            </Button>
+          </form>
         </Emailbox>
         <Bottomdiv>
           <BottomText>Don’t have an account?</BottomText>
---
+++ client/src/Pages/Modal/Discard.js
@@ -0,0 +1,118 @@
+import styled from "styled-components";
+import Button from "../../Components/style/Button";
+import XImg from "../../Components/style/img/tabler_x.png";
+
+const Container = styled.div`
+  width: 570px;
+  height: 190px;
+  z-index: 999;
+  position: absolute;
+  top: 30%;
+  left: 30%;
+  transfrom: translate(-50%, -50%);
+  background-color: #ffffff;
+  border-radius: 10px;
+  border 1px solid var(--black-100);
+  padding: 24px;
+`;
+
+const ModalContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  z-index: 900;
+  position: absolute;
+  top: 0%;
+  left: 0%;
+  transfrom: translate(-50%, -50%);
+  background-color: #000000;
+  padding: 30px;
+  opacity: 50%;
+`;
+
+const H1 = styled.span`
+  font-size: var(--font-xx-large);
+  font-weight: 700;
+  text-alignt: center;
+  color: var(--red-400);
+`;
+
+const Textdiv = styled.div`
+  width: 20px;
+  margin: 20px 0px;
+  height: 10px;
+`;
+const Buttondiv = styled.div`
+  display: flex;
+  margin-top: 40px;
+`;
+const Span = styled.span`
+  font-size: var(--font-large);
+`;
+const XBtn = styled.button`
+  width: 30px;
+  height: 30px;
+  background: #ffffff;
+  color: var(--black-500);
+  text-align: center;
+
+  &: hover {
+    background: var(--black-100);
+  }
+`;
+
+const Ximg = styled.img`
+  width: 20px;
+  height: 20px;
+`;
+
+const HeaderDiv = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+`;
+
+function Discard({ showModal }) {
+  return (
+    <>
+      <ModalContainer />
+      <Container>
+        <HeaderDiv>
+          <H1>Discard Question</H1>
+          <XBtn onClick={showModal}>
+            <Ximg src={XImg} alt="" />
+          </XBtn>
+        </HeaderDiv>
+
+        <Textdiv>
+          <Span>
+            Are you sure you want to discard this question? This cannot be
+            undone.
+          </Span>
+        </Textdiv>
+        <Buttondiv>
+          <Button
+            size="custom"
+            variant="Discard"
+            width="120px"
+            height="40px"
+            padding="3px 3px 3px 3px"
+            margin="0px 3px 0px 0px"
+          >
+            Discard Question
+          </Button>
+          <Button
+            size="custom"
+            variant="share"
+            width="120px"
+            height="40px"
+            onClick={showModal}
+          >
+            Cansel
+          </Button>
+        </Buttondiv>
+      </Container>
+    </>
+  );
+}
+
+export default Discard;
