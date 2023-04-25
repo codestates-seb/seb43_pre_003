056b4dcae8d6e6b9a0ea90776f78dcd73d00d0bb
moonozz <http0127@naver.com>
2023-04-25 11:29:05 +0900

    Feat:서버에 질문 글등록하기

--- client/src/util/api.js
+++ client/src/util/api.js
@@ -8,7 +8,7 @@ export const axiosCreate = (url, data) => {
     .post(url, data, {
       headers: {
         "Content-Type": "application/json",
-        Authorization: `Bearer ${process.env.REACT_APP_TOKEN}`,
+        Authorization: localStorage.getItem("token"),
       },
     })
     .then(() => {
