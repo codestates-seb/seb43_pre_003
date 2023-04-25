3b760bc84c4fd506c68c2fd51795c40b289c56a0
JoDaeUk <du39574@gmail.com>
2023-04-25 16:28:50 +0900

    회원탈퇴, 프로필수정 변경

--- server/src/main/java/com/seb43/preProject/member/controller/MemberController.java
+++ server/src/main/java/com/seb43/preProject/member/controller/MemberController.java
@@ -8,6 +8,7 @@ import com.seb43.preProject.member.service.MemberService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -52,7 +53,7 @@ public class MemberController {
     public ResponseEntity deleteMember(){
         Long memberId = memberService.findSecurityContextHolderMemberId();
         Member member = memberService.deleteMember(memberId);
-
+        SecurityContextHolder.clearContext(); //탈퇴후 로그아웃
         return new ResponseEntity<>((memberMapper.memberToMemberResponse(member)),HttpStatus.OK);
     }
 
